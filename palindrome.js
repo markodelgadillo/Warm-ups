a48e1c0b79ac8dd916eb0699ce5e4fa7da875a2e
marko delgadillo <markoneseven@gmail.com>
2017-09-23 19:29:19 -0700

    Added notes for every step.

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,14 @@
 function isPalindrome(string) {
-  var separated = string.split(['' ['']])
-  var wordArray = []
-  var word = ''
-    for (var i = separated.length; i >= 0; i--){
-      wordArray.push(separated[i])
-    }
-      word = wordArray.join('')
-      if (word === string){
-        return true
+  var separated = string.split(['' ['']])//separated the string into an array of characters
+  var wordArray = [] //empty array to push the characters from the string array above
+  var word = '' //variable to return the joined array into a single string
+    for (var i = separated.length; i >= 0; i--){ //for loop to start from the end and loop backwards
+      wordArray.push(separated[i])//pushes each character looped throuh into the empty array
+    }//ends loop
+      word = wordArray.join('')//the array with pushed characters is now joined into a single string
+      if (word === string){ //checks if the newly joined string matches the original string
+        return true //if yes, it is a palindrome
       } else {
-          return false
+          return false //if not, it is not a palindrome
         }
 }
