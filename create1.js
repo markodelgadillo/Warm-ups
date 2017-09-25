a1e3a79e666a6d278107201f2f512904fe1d3861
marko delgadillo <markoneseven@gmail.com>
2017-09-25 09:47:05 -0700

    Create Element part 1 first attempt.

---
+++ create1.js
@@ -0,0 +1,15 @@
+var object = {class: 'orange'};
+var $paragraph = document.createElement('p');
+
+function createElement(tagName, attributes) {
+  var el = ''
+  for (tagName in attributes){
+    el =
+
+    return el
+
+  }
+
+}
+
+createElement($paragraph,object)
