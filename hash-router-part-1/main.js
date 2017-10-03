d1d8f21461f4b46f476d5c7706d85dd19d30bbec
marko delgadillo <markoneseven@gmail.com>
2017-10-03 08:55:28 -0700

    hash router part 1 attempt.

--- hash-router-part-1/main.js
+++ hash-router-part-1/main.js
@@ -1,5 +1,19 @@
-window.addEventListener('hashchange', function (event) {
-  let id = event.target.getAttribute('id')
-  
+const fire = document.querySelector('#fire')
+const water = document.querySelector('#water')
+const grass = document.querySelector('#grass')
 
-})
+
+
+window.addEventListener('hashchange', function (event) {
+  const location  = event.target.window.location
+    if (location.hash === '#fire') {
+      fire.classList.toggle('hidden')
+    } else if (location.hash === '#water') {
+      water.classList.toggle('hidden')
+    } else if (location.hash === '#grass') {
+      grass.classList.toggle('hidden')
+    }
+  })
+
+
+//
